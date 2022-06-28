85f0407ce77637421b59b4f84632684d666c9a8d
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2022-06-28 15:40:56 -0500

    Estructura del proyecto

---
+++ business/src/main/java/co/sofka/com/App.java
@@ -0,0 +1,13 @@
+package co.sofka.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
