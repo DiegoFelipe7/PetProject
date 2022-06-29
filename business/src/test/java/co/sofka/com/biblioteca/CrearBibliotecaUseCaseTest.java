d6578ce2b51ab8b3c2707a9ea6800a50798c116d
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-29 16:48:21 -0500

    Agregando test para el caso de uso CrearBiblioteca

--- business/src/test/java/co/sofka/com/biblioteca/CrearBibliotecaUseCaseTest.java
+++ business/src/test/java/co/sofka/com/biblioteca/CrearBibliotecaUseCaseTest.java
@@ -1,10 +1,54 @@
 package co.sofka.com.biblioteca;
 
+import co.com.sofka.business.generic.UseCaseHandler;
+import co.com.sofka.business.repository.DomainEventRepository;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.domain.generic.DomainEvent;
+import co.sofka.com.biblioteca.commands.CrearBiblioteca;
+import co.sofka.com.biblioteca.events.BibliotecaCreada;
 import co.sofka.com.biblioteca.usecases.CrearBibliotecaUseCase;
-import org.junit.Test;
+import co.sofka.com.biblioteca.values.BibliotecaId;
+import co.sofka.com.biblioteca.values.BibliotecarioId;
+import co.sofka.com.prestamo.events.CambioDeEstadoEnRegistro;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class CrearBibliotecaUseCaseTest {
 
+    @Mock
+    DomainEventRepository repository;
+
+    @InjectMocks
     private CrearBibliotecaUseCase crearBibliotecaUseCase;
+
+    @Test
+    public void crearBibliotecaTest(){
+        //arrange
+        BibliotecaId bibliotecaId = new BibliotecaId("hola");
+        BibliotecarioId bibliotecarioId = new BibliotecarioId("hol");
+        Biblioteca biblioteca =  new Biblioteca(bibliotecaId, bibliotecarioId);
+        var command = new CrearBiblioteca(bibliotecaId,bibliotecarioId);
+
+
+        //act
+        var events = UseCaseHandler
+                .getInstance().syncExecutor(crearBibliotecaUseCase, new RequestCommand<>(command))
+                .orElseThrow()
+                .getDomainEvents();
+
+        //assert
+        var event = (BibliotecaCreada)events.get(0);
+        Assertions.assertEquals("hol", event.bibliotecarioId().value());
+    }
+
 
 }
