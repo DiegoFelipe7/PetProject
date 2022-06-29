97ae71b60cfb639edb6977a3fcdc7da82590561d
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-29 16:47:26 -0500

    Cambiando el llamado al constructor de Biblioteca

--- business/src/main/java/co/sofka/com/biblioteca/usecases/CrearBibliotecaUseCase.java
+++ business/src/main/java/co/sofka/com/biblioteca/usecases/CrearBibliotecaUseCase.java
@@ -5,7 +5,7 @@ import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
 import co.sofka.com.biblioteca.commands.CrearBiblioteca;
-import co.sofka.com.biblioteca.entities.Biblioteca;
+import co.sofka.com.biblioteca.Biblioteca;
 import co.sofka.com.biblioteca.values.BibliotecaId;
 
 public class CrearBibliotecaUseCase extends UseCase<RequestCommand<CrearBiblioteca>, ResponseEvents> {
@@ -14,8 +14,7 @@ public class CrearBibliotecaUseCase extends UseCase<RequestCommand<CrearBibliote
     @Override
     public void executeUseCase(RequestCommand<CrearBiblioteca> crearBibliotecaRequestCommand) {
         var command = crearBibliotecaRequestCommand.getCommand();
-        var biblioteca = new Biblioteca(new BibliotecaId());
+        var biblioteca = new Biblioteca(command.bibliotecaId(),command.bibliotecarioId());
         emit().onResponse(new ResponseEvents(biblioteca.getUncommittedChanges()));
-
     }
 }
