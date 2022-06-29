920775563797a54f2bae86392c225eb45446604c
Ricardo Ortega <tattortega.28@gmail.com>
2022-06-29 16:45:33 -0500

    Modificando el parametro del contructor CrearBiblioteca

--- domain/src/main/java/co/sofka/com/biblioteca/commands/CrearBiblioteca.java
+++ domain/src/main/java/co/sofka/com/biblioteca/commands/CrearBiblioteca.java
@@ -2,16 +2,23 @@ package co.sofka.com.biblioteca.commands;
 
 import co.com.sofka.domain.generic.Command;
 import co.sofka.com.biblioteca.values.BibliotecaId;
+import co.sofka.com.biblioteca.values.BibliotecarioId;
 
 public class CrearBiblioteca extends Command {
 
     private final BibliotecaId bibliotecaId;
+    private final BibliotecarioId bibliotecarioId;
 
-    public CrearBiblioteca(BibliotecaId bibliotecaId){
+    public CrearBiblioteca(BibliotecaId bibliotecaId, BibliotecarioId bibliotecarioId){
         this.bibliotecaId = bibliotecaId;
+        this.bibliotecarioId = bibliotecarioId;
     }
 
     public BibliotecaId bibliotecaId() {
         return bibliotecaId;
+    }
+
+    public BibliotecarioId bibliotecarioId() {
+        return bibliotecarioId;
     }
 }
